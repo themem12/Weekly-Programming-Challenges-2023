82fb651333ccd93ff3e2827d11342eb0e60d11f5
Guillermo Saavedra Dorantes <saaveguillermo@gmail.com>
2023-03-28 00:43:08 -0600

    [UPD] Model and way to set destination for navigation link in Main Menu

--- WeeklyChallenges2023/WeeklyChallenges2023/ContentView.swift
+++ WeeklyChallenges2023/WeeklyChallenges2023/ContentView.swift
@@ -8,81 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    let cells = [
-        ChallengesCells(title: "FizzBuzz"),
-        ChallengesCells(title: "L33t"),
-        ChallengesCells(title: "Tennis game"),
-        ChallengesCells(title: "Password Generator"),
-        ChallengesCells(title: "Prime Fibonachi or Pair"),
-        ChallengesCells(title: "Hola Mundo"),
-        ChallengesCells(title: "Rock Paper Sissors Lizard Spock"),
-        ChallengesCells(title: "Sorting Hat"),
-        ChallengesCells(title: "Pseudorandom"),
-        ChallengesCells(title: "Hete-Iso-Pag gram"),
-        ChallengesCells(title: "API"),
-        ChallengesCells(title: "URLParams"),
-        ChallengesCells(title: "Friday 13"),
+    private let cells = [
+        ChallengesCells(title: "FizzBuzz", viewClass: AnyView(FizzBuzzView())),
+        ChallengesCells(title: "L33t", viewClass: AnyView(L33tView())),
+        ChallengesCells(title: "Tennis game", viewClass: AnyView(TennisGameView())),
+        ChallengesCells(title: "Password Generator", viewClass: AnyView(PassWordGeneratorView())),
+        ChallengesCells(title: "Prime Fibonachi or Pair", viewClass: AnyView(PrimeFibonachiPairView())),
+        ChallengesCells(title: "Hola Mundo", viewClass: AnyView(HolaMundoView())),
+        ChallengesCells(title: "Rock Paper Sissors Lizard Spock", viewClass: AnyView(RPSLSView())),
+        ChallengesCells(title: "Sorting Hat", viewClass: AnyView(SortingHatView())),
+        ChallengesCells(title: "Pseudorandom", viewClass: AnyView(Pseudorandom())),
+        ChallengesCells(title: "Hete-Iso-Pag gram", viewClass: AnyView(Hete_Iso_PangramaView())),
+        ChallengesCells(title: "API", viewClass: AnyView(APIView())),
+        ChallengesCells(title: "URLParams", viewClass: AnyView(URLParamsView())),
+        ChallengesCells(title: "Friday 13", viewClass: AnyView(FridayThirteenView())),
     ]
+    
     var body: some View {
         NavigationView {
             List(cells) { cell in
-                switch cell.title {
-                case "FizzBuzz":
-                    NavigationLink(destination: FizzBuzzView()) {
-                        Text(cell.title)
-                    }
-                case "L33t":
-                    NavigationLink(destination: L33tView()) {
-                        Text(cell.title)
-                    }
-                case "Tennis game":
-                    NavigationLink(destination: TennisGameView()) {
-                        Text("\(cell.title)")
-                    }
-                case "Password Generator":
-                    NavigationLink(destination: PassWordGeneratorView()) {
-                        Text(cell.title)
-                    }
-                case "Prime Fibonachi or Pair":
-                    NavigationLink(destination: PrimeFibonachiPairView()) {
-                        Text(cell.title)
-                    }
-                case "Hola Mundo":
-                    NavigationLink(destination: HolaMundoView()) {
-                        Text("\(cell.title)")
-                    }
-                case "Rock Paper Sissors Lizard Spock":
-                    NavigationLink(destination: RPSLSView()) {
-                        Text(cell.title)
-                    }
-                case "Sorting Hat":
-                    NavigationLink(destination: SortingHatView()) {
-                        Text("\(cell.title)")
-                    }
-                case "Pseudorandom":
-                    NavigationLink(destination: Pseudorandom()) {
-                        Text(cell.title)
-                    }
-                case "Hete-Iso-Pag gram":
-                    NavigationLink(destination: Hete_Iso_PangramaView()) {
-                        Text(cell.title)
-                    }
-                case "API":
-                    NavigationLink(destination: APIView()) {
-                        Text("\(cell.title)")
-                    }
-                case "URLParams":
-                    NavigationLink(destination: URLParamsView()) {
-                        Text("\(cell.title)")
-                    }
-                case "Friday 13":
-                    NavigationLink(destination: FridayThirteenView()) {
-                        Text(cell.title)
-                    }
-                default:
-                    NavigationLink(destination: FizzBuzzView()) {
-                        Text(cell.title)
-                    }
+                NavigationLink(destination: cell.viewClass) {
+                    Text(cell.title)
                 }
             }
             .navigationBarTitle("Retos semanales 2023")
@@ -98,5 +44,6 @@ struct ContentView_Previews: PreviewProvider {
 
 struct ChallengesCells: Identifiable {
     let title: String
+    let viewClass: AnyView
     let id = UUID()
 }
