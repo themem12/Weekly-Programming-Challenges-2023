9ca60e8c868dab4f0aedea9d81c6e84658688f35
themem12 <saaveguillermo@gmail.com>
2024-12-09 17:01:12 -0600

    Add view for challenge

---
+++ WeeklyChallenges2023/Views/35-FirstSteps/FirstStepsView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstStepsView.swift
+//  WeeklyChallenges2023
+//
+//  Created by Saavedra, Guillermo on 09/12/24.
+//
+
+import SwiftUI
+
+struct FirstStepsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstStepsView()
+}
