b48ef81c7121580a105d5c56ab04f791c059abae
Guillermo Saavedra Dorantes <saaveguillermo@gmail.com>
2024-12-14 20:34:12 -0600

    Add view for challenge and fix tests

--- WeeklyChallenges2023Tests/AddsViewModelTests.swift
+++ WeeklyChallenges2023Tests/AddsViewModelTests.swift
@@ -12,8 +12,24 @@ final class AddsViewModelTests: XCTestCase {
 
     let sut = AddsViewModel()
 
-    func test_() {
+    func test_withCorrectText_shouldReturn_correctCombinations() {
+        sut.numbersText = "1.5.3.2"
+        sut.targetText = "6"
         sut.startSearch()
         XCTAssertEqual(sut.combinations, [[1,5], [1,3,2]])
+    }
+
+    func test_withIncorrectText_shouldReturn_emptyCombinations() {
+        sut.numbersText = "1,5,3,2"
+        sut.targetText = "6"
+        sut.startSearch()
+        XCTAssertEqual(sut.combinations, [])
+    }
+
+    func test_withIncorrectTargetText_shouldReturn_emptyCombinations() {
+        sut.numbersText = "1.5.3.2"
+        sut.targetText = "6.5"
+        sut.startSearch()
+        XCTAssertEqual(sut.combinations, [])
     }
 }
