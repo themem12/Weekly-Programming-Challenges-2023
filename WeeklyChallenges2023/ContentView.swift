0b2c1d79df71de9813a5f434f18917824c4ea70c
Guillermo Saavedra Dorantes <37200661+themem12@users.noreply.github.com>
2024-12-18 22:58:07 -0600

    [CHALLENGE] Add challenge 44 Math quiz (#22)
    Add complete challenge 44
    Update README

--- WeeklyChallenges2023/ContentView.swift
+++ WeeklyChallenges2023/ContentView.swift
@@ -55,6 +55,7 @@ struct ContentView: View {
         ChallengesCells(title: "39 - Pythagorean Triples", viewClass: AnyView(PythagoreanTriplesView())),
         ChallengesCells(title: "40 - Multiplication Tables", viewClass: AnyView(MultiplicationTablesView())),
         ChallengesCells(title: "43 - Weather Simulator", viewClass: AnyView(WeatherSimulatorView())),
+        ChallengesCells(title: "44 - Math quiz", viewClass: AnyView(MathQuizView())),
         ChallengesCells(title: "45 - Advent calendar", viewClass: AnyView(AdventCalendarView())),
         ChallengesCells(title: "47 - Hundred points word", viewClass: AnyView(HundredPointWordView())),
     ]
