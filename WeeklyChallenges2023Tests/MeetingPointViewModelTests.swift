a76e11e887c28853a99134f3abd29cc025e76280
themem12 <saaveguillermo@gmail.com>
2025-01-09 15:45:47 -0600

    Add missing stuff for merging

--- WeeklyChallenges2023Tests/MeetingPointViewModelTests.swift
+++ WeeklyChallenges2023Tests/MeetingPointViewModelTests.swift
@@ -23,7 +23,7 @@ final class MeetingPointViewModelTests: XCTestCase {
         sut.xfLineBText = "2"
         sut.yfLineBText = "-5"
 
-        sut.setLines()
+        sut.getPoint()
 
         XCTAssertEqual(sut.firstLine?.equation, "y=-8.0x+11.0")
         XCTAssertEqual(sut.firstLine?.m, -8.0)
@@ -32,9 +32,8 @@ final class MeetingPointViewModelTests: XCTestCase {
         XCTAssertEqual(sut.secondLine?.m, -2.3333333)
         XCTAssertEqual(sut.secondLine?.b, -0.33333334)
 
-        let result = sut.resolveEquationSystem()
-        XCTAssertEqual(result?.0, 1.9999999)
-        XCTAssertEqual(result?.1, -4.999999)
+        XCTAssertEqual(sut.xMeetingPoint, "x: 1.9999999")
+        XCTAssertEqual(sut.yMeetingPoint, "y: -4.999999")
     }
 
     func test_withPerpendicularLinesValues_shouldReturn_results() {
@@ -48,7 +47,7 @@ final class MeetingPointViewModelTests: XCTestCase {
         sut.xfLineBText = "2"
         sut.yfLineBText = "2"
 
-        sut.setLines()
+        sut.getPoint()
 
         XCTAssertEqual(sut.firstLine?.equation, "y=0.0x+3.0")
         XCTAssertEqual(sut.firstLine?.m, 0.0)
@@ -57,7 +56,6 @@ final class MeetingPointViewModelTests: XCTestCase {
         XCTAssertEqual(sut.secondLine?.m, 0.0)
         XCTAssertEqual(sut.secondLine?.b, 2.0)
 
-        let _ = sut.resolveEquationSystem()
         XCTAssertTrue(sut.error)
         XCTAssertEqual(sut.errorMessage, "Lines won't meet")
     }
